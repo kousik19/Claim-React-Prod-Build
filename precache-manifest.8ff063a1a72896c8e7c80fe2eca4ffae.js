77b83249b3b3772589822e94bc8b5f9b369a93a4
Kousik Mandal <kousik348@gmail.com>
2021-01-28 11:43:46 +0530

    28-01-2021

---
+++ precache-manifest.8ff063a1a72896c8e7c80fe2eca4ffae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7c09dc1f793d82c1975e42815f1982",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "abab7f7e7582087a7751",
+    "url": "/portal/static/css/2.7a343859.chunk.css"
+  },
+  {
+    "revision": "e4e445b66a6ae6849a76",
+    "url": "/portal/static/css/main.3bddb786.chunk.css"
+  },
+  {
+    "revision": "abab7f7e7582087a7751",
+    "url": "/portal/static/js/2.41c44b5c.chunk.js"
+  },
+  {
+    "revision": "c31450b7885f5fb61b553b542dac982e",
+    "url": "/portal/static/js/2.41c44b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e445b66a6ae6849a76",
+    "url": "/portal/static/js/main.05018686.chunk.js"
+  },
+  {
+    "revision": "5bc996b8223c7fe259bf",
+    "url": "/portal/static/js/runtime-main.4ed82520.js"
+  },
+  {
+    "revision": "9901eaed2638bbdcdba4ff448663ec2b",
+    "url": "/portal/static/media/header3.9901eaed.jpg"
+  },
+  {
+    "revision": "48982a622066ba46c4195e7f9e2f4a3b",
+    "url": "/portal/static/media/kmr-logo.48982a62.png"
+  },
+  {
+    "revision": "0dbfd12e0d6a5f55c7b71237cdafad6a",
+    "url": "/portal/static/media/local-logo.0dbfd12e.png"
+  },
+  {
+    "revision": "71721f1e369814ca1825deb51201a4e9",
+    "url": "/portal/static/media/login-form-bg.71721f1e.jpg"
+  },
+  {
+    "revision": "c18034833c27e7c670174e2421f34748",
+    "url": "/portal/static/media/login_bg.c1803483.png"
+  }
+]);
